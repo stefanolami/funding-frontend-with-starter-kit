cb343c806f247f2c3e4f2783278e8596786bcc6e
stefanolami <stefanolami90@gmail.com>
2023-03-24 17:29:25 +0200

    set up project and created header and footer components

--- src/test/test.component.ts
+++ src/test/test.component.ts
@@ -2,7 +2,7 @@ import { html } from "lit";
 import { customElement, property } from "lit/decorators.js";
 import { TailwindElement } from "../shared/tailwind.element";
 
-import style from "./test.component.scss?inline";
+import style from "../shared/tailwind.global.css";
 
 @customElement("test-component")
 export class TestComponent extends TailwindElement(style) {
@@ -11,12 +11,12 @@ export class TestComponent extends TailwindElement(style) {
 
   render() {
     return html`
-      <p>
+      <p class="bg-green-200">
         Hello,
         <b>${this.name}</b>
         !
       </p>
-      <button class="bg-blue-200 text-yellow-200 p-2 rounded-full text-2xl">Hello world!</button>
+      <button class="bg-red-500 text-yellow-200 p-2 rounded-full text-2xl">Hello world!</button>
     `;
   }
 }
