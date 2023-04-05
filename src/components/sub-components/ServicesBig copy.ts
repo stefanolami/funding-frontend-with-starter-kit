d779a0f2732d942ef18c5a76259f40bd4ddcaf5f
stefanolami <stefanolami90@gmail.com>
2023-04-05 19:50:52 +0300

    added services tablet/desktop

--- src/components/sub-components/ServicesBig copy.ts
+++ src/components/sub-components/ServicesBig copy.ts
@@ -1,6 +1,5 @@
 import { html } from "lit";
-import { customElement, state } from "lit/decorators.js";
-import { classMap } from 'lit/directives/class-map.js';
+import { customElement } from "lit/decorators.js";
 import { TailwindElement } from "../../shared/tailwind.element";
 
 import style from "../../shared/tailwind.global.css";
@@ -8,8 +7,6 @@ import style from "../../shared/tailwind.global.css";
 @customElement("services-big")
 export class ServicesBig extends TailwindElement(style) {
 
-    @state()
-    activeArr: boolean[] = [false, false, false, false, false, false, false, false, false, false, false, false]
 
     render() {
         return html`
@@ -25,24 +22,24 @@ export class ServicesBig extends TailwindElement(style) {
                     <div class="absolute z-10 -top-32 xl:-top-16 left-6 xl:left-16 w-[160px] text-mygrey/80 text-center font-unna-700 text-[31px] leading-[36px]">PROJECT CLOSURE</div>
 
 
-                    <div @mouseover="${() => this.showCard(0)}" class="-top-16 xl:-top-11 left-[310px] xl:left-[445px] tablet-services-btns ${classMap({'bg-mygreen-light': this.activeArr[0] == false, 'bg-mygreen': this.activeArr[0] == true})}">Brainstorming</div>
-                    <div @mouseover="${() => this.showCard(1)}" class="top-3 right-36 xl:right-60 tablet-services-btns ${classMap({'bg-mygreen-light': this.activeArr[1] == false, 'bg-mygreen': this.activeArr[1] == true})}">Idea Development</div>
-                    <div @mouseover="${() => this.showCard(2)}" class="top-[120px] xl:top-36 right-8 xl:right-24 tablet-services-btns ${classMap({'bg-mygreen-light': this.activeArr[2] == false, 'bg-mygreen': this.activeArr[2] == true})}">Funding Match</div>
-                    <div @mouseover="${() => this.showCard(3)}" class="top-64 xl:top-[300px] right-8 xl:right-16 tablet-services-btns ${classMap({'bg-primary': this.activeArr[3] == false, 'bg-primary-focus': this.activeArr[3] == true})}">Idea to Project</div>
-                    <div @mouseover="${() => this.showCard(4)}" class="bottom-[72px] xl:bottom-24 right-[90px] xl:right-40 tablet-services-btns ${classMap({'bg-primary': this.activeArr[4] == false, 'bg-primary-focus': this.activeArr[4] == true})}">Project Development</div>
-                    <div @mouseover="${() => this.showCard(5)}" class="-bottom-7 right-52 xl:right-[330px] tablet-services-btns ${classMap({'bg-primary': this.activeArr[5] == false, 'bg-primary-focus': this.activeArr[5] == true})}">Project Proposal</div>
-                    <div @mouseover="${() => this.showCard(6)}" class="-bottom-7 left-[215px] xl:left-[345px] tablet-services-btns ${classMap({'bg-primary-dark2': this.activeArr[6] == false, 'bg-primary-dark2/80': this.activeArr[6] == true})}">Contract Negotiation</div>
-                    <div @mouseover="${() => this.showCard(7)}" class="bottom-[72px] xl:bottom-24 left-[106px] xl:left-44 tablet-services-btns ${classMap({'bg-primary-dark2': this.activeArr[7] == false, 'bg-primary-dark2/80': this.activeArr[7] == true})}">Project Implementation</div>
-                    <div @mouseover="${() => this.showCard(8)}" class="top-64 xl:top-[300px] left-10 xl:left-20 tablet-services-btns ${classMap({'bg-primary-dark2': this.activeArr[8] == false, 'bg-primary-dark2/80': this.activeArr[8] == true})}">Project Communication</div>
-                    <div @mouseover="${() => this.showCard(9)}" class="top-[120px] xl:top-36 left-10 xl:left-28 tablet-services-btns ${classMap({'bg-mygrey/80': this.activeArr[9] == false, 'bg-mygrey': this.activeArr[9] == true})}">Completion of Project</div>
-                    <div @mouseover="${() => this.showCard(10)}" class="-top-8 xl:top-6 left-0 xl:-left-20 tablet-services-btns ${classMap({'bg-mygrey/80': this.activeArr[10] == false, 'bg-mygrey': this.activeArr[10] == true})}">Market Introduction</div>
-                    <div @mouseover="${() => this.showCard(11)}" class="top-3 left-40 xl:left-60 tablet-services-btns ${classMap({'bg-mygrey/80': this.activeArr[11] == false, 'bg-mygrey': this.activeArr[11] == true})}">New Possibilities</div>
+                    <div class="-top-16 xl:-top-11 left-[310px] xl:left-[445px] tablet-services-btns tablet-project-initiation-btns services-project-initiation unfocused">Brainstorming</div>
+                    <div class="top-3 right-36 xl:right-60 tablet-services-btns tablet-project-initiation-btns services-project-initiation unfocused">Idea Development</div>
+                    <div class="top-[120px] xl:top-36 right-8 xl:right-24 tablet-services-btns tablet-project-initiation-btns services-project-initiation unfocused">Funding Match</div>
+                    <div class="top-64 xl:top-[300px] right-8 xl:right-16 tablet-services-btns tablet-project-planning-btns services-project-planning unfocused">Idea to Project</div>
+                    <div class="bottom-[72px] xl:bottom-24 right-[90px] xl:right-40 tablet-services-btns tablet-project-planning-btns services-project-planning unfocused">Project Development</div>
+                    <div class="-bottom-7 right-52 xl:right-[330px] tablet-services-btns tablet-project-planning-btns services-project-planning unfocused">Project Proposal</div>
+                    <div class="-bottom-7 left-[215px] xl:left-[345px] tablet-services-btns tablet-project-execution-btns services-project-execution unfocused">Contract Negotiation</div>
+                    <div class="bottom-[72px] xl:bottom-24 left-[106px] xl:left-44 tablet-services-btns tablet-project-execution-btns services-project-execution unfocused">Project Implementation</div>
+                    <div class="top-64 xl:top-[300px] left-10 xl:left-20 tablet-services-btns tablet-project-execution-btns services-project-execution unfocused">Project Communication</div>
+                    <div class="top-[120px] xl:top-36 left-10 xl:left-28 tablet-services-btns tablet-project-closure-btns services-project-closure unfocused">Completion of Project</div>
+                    <div class="-top-8 xl:top-6 left-0 xl:-left-20 tablet-services-btns tablet-project-closure-btns services-project-closure unfocused">Market Introduction</div>
+                    <div class="top-3 left-40 xl:left-60 tablet-services-btns tablet-project-closure-btns services-project-closure unfocused">New Possibilities</div>
 
 
-                    <img id="tablet-services-logo" class="${classMap({'hidden': this.activeArr.includes(true), 'block': !this.activeArr.includes(true)})} w-[425px] absolute z-10 top-20 xl:top-40 left-[178px] xl:left-80" src="../../images/our-services/T&P-Logo-services-tablet.png" alt="">
+                    <img id="tablet-services-logo" class="block w-[425px] absolute z-10 top-20 xl:top-40 left-[178px] xl:left-80" src="../../images/our-services/T&P-Logo-services-tablet.png" alt="">
 
 
-                    <div class="${classMap({'hidden': this.activeArr[0] == false, 'block': this.activeArr[0] == true})} absolute tablet-services-div tablet-project-initiation top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-initiation top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title  text-mygreen">BRAINSTORMING</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Creativity process to gather ideas, thoughts and inspiration</li>
@@ -52,7 +49,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[1] == false, 'block': this.activeArr[1] == true})} absolute tablet-services-div tablet-project-initiation top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-initiation top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-mygreen">IDEA DEVELOPMENT</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Modification of the ideas / sorting out the best opportunities from the idea catalogue</li>
@@ -62,7 +59,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[2] == false, 'block': this.activeArr[21] == true})} absolute tablet-services-div tablet-project-initiation top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-initiation top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-mygreen">FUNDING MATCH</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Searching the funding landscape for opportunities matching your idea</li>
@@ -73,7 +70,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[3] == false, 'block': this.activeArr[3] == true})} absolute tablet-services-div tablet-project-planning top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-planning top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-primary-focus">IDEA TO PROJECT</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Breaking the idea down to major development stages, showing where or what development is needed to go from state-of-the-art 
@@ -85,7 +82,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[4] == false, 'block': this.activeArr[4] == true})} absolute tablet-services-div tablet-project-planning top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-planning top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-primary-focus">PROJECT DEVELOPMENT</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Building project structure and defining work-packages and tasks</li>
@@ -96,7 +93,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[5] == false, 'block': this.activeArr[5] == true})} absolute tablet-services-div tablet-project-planning top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-planning top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-primary-focus">PROJECT PROPOSAL</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Managing the writing process, building project budget, proofreading input from partners, verification of alignment with EU national 
@@ -107,7 +104,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[6] == false, 'block': this.activeArr[6] == true})} absolute tablet-services-div tablet-project-execution top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-execution top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-primary-dark2/80">CONTRACT NEGOTIATION</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Responding to funding offer modifications in project description, according to offer</li>
@@ -118,7 +115,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[7] == false, 'block': this.activeArr[7] == true})} absolute tablet-services-div tablet-project-execution top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-execution top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-primary-dark2/80">PROJECT IMPLEMENTATION</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Progress monitoring and management, including organising project meetings and the general assembly</li>
@@ -128,7 +125,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[8] == false, 'block': this.activeArr[8] == true})} absolute tablet-services-div tablet-project-execution top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-execution top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-primary-dark2/80">PROJECT COMMUNICATION</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Internal communication management – e.g. data storage and email</li>
@@ -138,7 +135,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[9] == false, 'block': this.activeArr[9] == true})} absolute tablet-services-div tablet-project-closure top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-closure top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-mygrey">COMPLETION OF PROJECT</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Final reporting</li>
@@ -149,7 +146,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[10] == false, 'block': this.activeArr[10] == true})} absolute tablet-services-div tablet-project-closure top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-closure top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-mygrey">MARKET INTRODUCTION</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Product introduction to market – i.e. development of business plan, market penetration strategy, marketing management strategy and product assortment (market) strategy</li>
@@ -158,7 +155,7 @@ export class ServicesBig extends TailwindElement(style) {
                             <div>GET IN TOUCH</div>
                         </a>
                     </div>
-                    <div class="${classMap({'hidden': this.activeArr[11] == false, 'block': this.activeArr[11] == true})} absolute tablet-services-div tablet-project-closure top-32 xl:top-[200px] left-[240px] xl:left-[330px] z-20 w-[312px] xl:w-[402px] xl:h-[340px]">
+                    <div class="hidden absolute tablet-services-div tablet-project-closure top-44 xl:top-[200px] left-[210px] xl:left-[330px] z-20 w-[402px] h-[340px]">
                         <h6 class="tablet-services-div-title text-mygrey">NEW POSSIBILITIES</h6>
                         <ul class="font-jose-400 text-[17px] xl:text-[21px] leading-[21px] xl:leading-[24px] list-disc pl-4 xl:pl-6">
                             <li>Inspiration for further development – i.e. start the process all over again, with or without the brainstorming to extract and expand on the ideas coming out from the project.</li>
@@ -170,15 +167,6 @@ export class ServicesBig extends TailwindElement(style) {
                 </div>
             </div>                
         `;
-    }
-
-    showCard(i) {
-        const activeCardindex = this.activeArr.findIndex(element => element == true)
-        if (activeCardindex !== -1) {
-            this.activeArr[activeCardindex] = false
-        }
-        this.activeArr[i] = true
-        this.requestUpdate()
     }
 
 }
